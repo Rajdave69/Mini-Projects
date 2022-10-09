6768d53bf2ba757d31ed350c9786a5e78fd3efe7
Raj Dave <10864@bpsdoha.edu.qa>
2022-10-09 15:21:54 +0300

    update cloudflare ddns to not crash when there is no wifi

--- Cloudflare DDNS/main.py
+++ Cloudflare DDNS/main.py
@@ -7,25 +7,45 @@ It's super easy to configure and deploy too! Just run it as a daemon.
 from cloudflare_ddns import CloudFlare
 import time
 
+
 email = ""    # Your cloudflare Email
 api_key = ""  # Needs to be the "Global API Key" from https://dash.cloudflare.com/profile/api-tokens
 domain = ""   # The domain you want DDNS to be for
 
-cf = CloudFlare(email, api_key, domain)
+
+try:
+    cf = CloudFlare(email, api_key, domain)
+except:
+    while True:
+        try:
+            cf = CloudFlare(email, api_key, domain)
+            break
+        except:
+            print("Can not establish connection to CloudFlare. Retrying in 60 seconds")
+            time.sleep(60)
 
 
 def update_dns():
     try:
         print(time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()), end=" | ")
         e = cf.sync_dns_from_my_ip()
+                
+    except Exception as e:
+        print(time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()), end=" | ")
+        print(e)
+        
+    
+
+
+while True:
+    try:
+        update_dns()
+    except SystemExit:
+        pass
         
     except Exception as e:
         print(time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()), end=" | ")
         print(e)
-    
-print("Started")
+        
 
-while True:
-    update_dns()
     time.sleep(300)
-
